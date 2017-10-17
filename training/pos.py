81cd23b38999c25d2a9ff9ca42d01cec58198136
opencv_learner <10054053@qq.com>
2017-10-17 16:55:09 +0800

    1
    1

--- training/pos.py
+++ training/pos.py
@@ -1,8 +1,19 @@
-import os,sys
+import os,sys,cv2
 directory_pos='c:/temp/pos'
 directory_neg='c:/temp/neg'
 os.chdir('c:/temp/')
-def pos():
+def resize(img,size):
+    pic=cv2.imread(img)
+    if pic.shape==(24,24,3):
+
+        print ('y',pic.shape)
+    else:
+        re=cv2.resize(pic,(size,size))
+        print ('n',pic.shape)
+        cv2.imwrite(img,re)
+
+    
+def pos_text():
     with open("pos.txt","w") as f:
         for b in os.listdir(directory_pos):
             print ('pos'+'/'+b+' 1 0 0 136 36')
@@ -14,8 +25,18 @@ def neg():
             print ('pos'+'/'+b+'\n')
 
             f.write('neg'+'/'+b+'\n')
-pos()
+def pos_resize():
+    os.chdir('c:/temp/pos/')
+    for b in os.listdir(directory_pos):
+        resize(b,24)
+def neg_resize():
+    os.chdir('c:/temp/neg/')
+    for b in os.listdir(directory_neg):
+        resize(b,24)
+pos_text()
+pos_resize()
 neg()
+neg_resize()
 os.system('opencv_createsamples.exe -info pos.txt -bg bg.txt -num 1400 -vec pos.vec')
 print ('done')
 #os.system('opencv_traincascade.exe -data dt1 -vec pos.vec -bg bg.txt -numPos 1400 -numNeg 1964 -numStages 16 -precalcValbufSize 2000 -precalcdxBufSize 4000 -featureType LBP -w 100 -h 100')
