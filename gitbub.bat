6067ce1a8e0b95316c5b20a57b9a3c57a858f099
wjb711 <10054053@qq.com>
2018-03-06 11:54:36 +0800

    123

---
+++ gitbub.bat
@@ -0,0 +1,5 @@
+set https_proxy=http://wanjianb:Beijing123_@web-gate4a.accounts.intern:3128
+rem git add git commit git push
+set GIT_SSL_NO_VERIFY=true git clone
+rem git clone -q https://github.com/wjb711/storage.git
+pause
