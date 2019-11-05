c0678884de513535dc32ac247c6b2b297ac93705
opencv_learner <10054053@qq.com>
2019-11-05 14:52:12 +0800

    Update ftp_server.py

--- office/ftp_server.py
+++ office/ftp_server.py
@@ -1,3 +1,25 @@
+'''
+pyftpdlib内部使用utf8，而windows使用gbk，可以将pyftpdlib进行修改：
+
+1、filesystems.py
+
+AbstractedFS.format_list与AbstractedFS.format_list最后一行
+
+yield line.encode('utf8', self.cmd_channel.unicode_errors)
+
+utf8改为gbk
+
+ 
+
+2、handlers.py
+
+FTPHandler.decode
+
+return bytes.decode('utf8', self.unicode_errors)
+
+utf8改为gbk
+
+'''
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
